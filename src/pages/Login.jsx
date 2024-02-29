ad6c79fdd53d407824faf6d0d186f7b5b0a81baa
makreno <makreno@gmail.com>
2024-02-29 15:43:37 -0600

    login 99%

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,46 +6,55 @@ export const Login = () => {
         <div className="flex items-center justify-items-center flex-col">
           <a>
             <img
-              className="object-contain w-16 h-12 mt-6"
+              className="object-contain w-16 h-12 mt-7"
               src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/original_logo_0DliJcfsTcciZen38gX9.png"
               alt="logo"
               class="logInLogo"
             />
           </a>
 
-          <h2 className="mb-2 mt-9 text-3xl font-semibold">
+          <h2 className="mb-2 mt-8 text-3xl font-semibold">
             Join the DEV Community
           </h2>
-          <p className="text-gray25 mt-0 p-0">
+          <p className="text-gray25 font-light mt-0 p-0">
             DEV Community is a community of 1,237,804 amazing developers
           </p>
         </div>
         <div className="flex items-center justify-items-center flex-col w-4/5 mt-4">
-          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1">
-            <Apple />
+          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md gap-1 hover:bg-gray-200">
+            <div className="ml-2">
+              <Apple />
+            </div>
+
             <span className="text-sm text-center w-full">
               Continue with Apple
             </span>
           </button>
-          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1">
-            <Forem />
+          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1 hover:bg-gray-200">
+            <div className="ml-2">
+              <Forem />
+            </div>
             <span className="text-sm text-center w-full">
               Continue with Forem
             </span>
           </button>
-          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1">
-            <Github />
+          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1 hover:bg-gray-200">
+            <div className="ml-2">
+              <Github />
+            </div>
             <span className="text-sm text-center w-full">
               Continue with Github
             </span>
           </button>
-          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1">
-            <Twitter />
+          <button className="flex items-center w-6/12 h-12 bg-transparent border border-neutral-300 rounded-md m-1 gap-1 hover:bg-gray-200">
+            <div className="ml-2">
+              <Twitter />
+            </div>
             <span className="text-sm text-center w-full">
               Continue with Twitter
             </span>
           </button>
-          <div className="mt-4 text-sm">OR</div>
+          <div className="mt-4 mb-4 text-sm">OR</div>
 
           <div id="div formLogIn" className="w-6/12">
             <form
@@ -54,13 +63,13 @@ export const Login = () => {
             >
               <label>Email</label>
               <input
-                className="bg-transparent border border-neutral-300 gap-1 rounded-md m-1 h-12 mt-4"
+                className="bg-transparent border border-neutral-300 gap-1 rounded-md m-1 h-9 mt-4"
                 type="email"
                 id="email"
               />
               <label>Password</label>
               <input
-                className="bg-transparent border border-neutral-300 gap-1 rounded-md m-1 h-12 mt-4"
+                className="bg-transparent border border-neutral-300 gap-1 rounded-md m-1 h-9 mt-4"
                 type="password"
                 id="password"
               />
@@ -71,11 +80,43 @@ export const Login = () => {
                     Remember Me
                   </label>
                 </div>
-                <label className="">Forgot password?</label>
+                <label className="text-blueButton font-light">
+                  <a href="">Forgot password?</a>
+                </label>
               </div>
-              <button className="flex items-center justify-center w-full h-12 bg-blueButton border border-neutral-300 mx-auto rounded-md">
-                <span className="text-">Log In</span>
+              <button className="flex items-center justify-center w-full h-12 bg-blueButton hover:bg-blue-800 border border-neutral-300 mx-auto rounded-md">
+                <span className="text-white">Log In</span>
               </button>
+              <div className="w-full gap-1 flex justify-center my-6">
+                <span className="text-gray-600 w-9/12 text-sm italic antialiased font-light text-center">
+                  By signing in, you are agreeing to our
+                  <a className="text-blueButton" href="https://dev.to/privacy">
+                    privacy policy
+                  </a>
+                  ,
+                  <a className="text-blueButton" href="https://dev.to/terms">
+                    terms of use
+                  </a>
+                  and
+                  <a
+                    className="text-blueButton"
+                    href="https://dev.to/code-of-conduct"
+                  >
+                    code of conduct.
+                  </a>
+                </span>
+              </div>
+              <div className="w-full gap-1 flex justify-center mt-6 mb-20">
+                <span className="font-light">
+                  New to DEV Community?{" "}
+                  <a
+                    className="text-blueButton"
+                    href="https://dev.to/enter?state=new-user"
+                  >
+                    Create account.
+                  </a>
+                </span>
+              </div>
             </form>
           </div>
         </div>
